826c7760108af0452d1ffb6f904069c483539001
brcelso <celso.sjunior@outlook.com>
2023-04-21 02:36:12 -0300

    ok

--- config/site.ts
+++ config/site.ts
@@ -1,3 +1,42 @@
+{/*
+
+import { NavItem } from "@/types/nav"
+
+interface SiteConfig {
+  name: string
+  description: string
+  mainNav: NavItem[]
+  links: {
+    twitter: string
+    github: string
+    docs: string
+    instagram: string
+    vercel: string
+    whatsapp: string
+  }
+}
+
+export const siteConfig: SiteConfig = {
+  name: "brcelso",
+  description:
+    "Beautifully designed components built with Radix UI and Tailwind CSS.",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+  ],
+  links: {
+    twitter: "https://twitter.com/juCa0514",
+    github: "https://github.com/brcelso",
+    docs: "https://ui.shadcn.com",
+    instagram: "https://instagram.com/celsosilvabr",
+    vercel: "https://vercel.com/brcelso",
+    whatsapp: "https://wa.me/5511972509876"
+  },
+}
+*/}
+
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
